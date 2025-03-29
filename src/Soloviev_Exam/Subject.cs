d686697f290b5f863760ff4e97d70e777a54c42f
mokroblin <neoflexmanparty@gmail.com>
2025-03-29 11:52:11 +0300

    add Subject.cs

---
+++ src/Soloviev_Exam/Subject.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Soloviev_Exam
+{
+    public class Subject
+{
+    public string Name { get; set; }
+    public string Lecturer { get; set; }
+    public int Semester { get; set; }
+
+    public Subject(string name, string lecturer, int semester)
+    {
+        Name = name;
+        Lecturer = lecturer;
+        Semester = semester;
+    }
+}
+
+}
